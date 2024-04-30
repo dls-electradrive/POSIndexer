012c13ce75e0b65a00c75bf2ff5036f160731ef0
Bjørn <bjand49@gmail.com>
2024-04-30 12:23:54 +0200

    Added validation for migrations

--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 using Microsoft.EntityFrameworkCore;
 using POSIndexer;
 using POSIndexer.Migrations;
+using System;
 
-var db = new InventoryReadDB();
-db.Database.Migrate();
-db.Dispose();
-
+var context = new InventoryReadDB();
+if (context.Database.GetPendingMigrations().Any())
+{
+    Console.WriteLine($"Found {context.Database.GetPendingMigrations().Count()} migrations");
+    context.Database.Migrate();
+}
+else
+{
+    Console.WriteLine("No migraitions to migrate");
+}
+context.Dispose();
 var messageHandler = new MQHandler();
 messageHandler.AttachCreateEvent();
 messageHandler.AttachRemoveEvent();
